3bbf2f939a64d3e5b076f6042acb61d75e0ddc9e
RoyceLuo1216 <86991110+RoyceLuo1216@users.noreply.github.com>
2024-10-29 16:09:12 -0400

    Finished Flexible Event Class

--- backend/src/EventEntity/Event.java
+++ backend/src/EventEntity/Event.java
@@ -1,3 +1,5 @@
+package EventEntity;
+
 import java.time.LocalDateTime;
 
 /**
@@ -5,12 +7,21 @@ import java.time.LocalDateTime;
  */
 public interface Event {
 
+    String setEventName();
+
     /**
      * Gets the name of the event.
      *
      * @return the name of the event as a String.
      */
-    String getName();
+    String getEventName();
+
+    /**
+     * Sets the name of the event.
+     *
+     * @param name the new name of the event.
+     */
+    void setEventName(String name);
 
     /**
      * Gets the start date and time of the event.
@@ -20,6 +31,13 @@ public interface Event {
     LocalDateTime getDayStart();
 
     /**
+     * Sets the start date and time of the event.
+     *
+     * @param dayStart the new start date and time of the event.
+     */
+    void setDayStart(LocalDateTime dayStart);
+
+    /**
      * Gets the end date and time of the event.
      *
      * @return the end time of the event as a LocalDateTime object.
@@ -27,9 +45,23 @@ public interface Event {
     LocalDateTime getDayEnd();
 
     /**
+     * Sets the end date and time of the event.
+     *
+     * @param dayEnd the new end date and time of the event.
+     */
+    void setDayEnd(LocalDateTime dayEnd);
+
+    /**
      * Gets the priority label of the event.
      *
      * @return the priority label of the event as an integer.
      */
     int getPriorityLabel();
+
+    /**
+     * Sets the priority label of the event.
+     *
+     * @param priorityLabel the new priority label of the event.
+     */
+    void setPriorityLabel(int priorityLabel);
 }
