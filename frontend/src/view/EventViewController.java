021d8e2d62634d6362e230518870b0dc6485fc36
Vennise Ho <venniseho14@gmail.com>
2024-11-13 17:03:28 -0500

    refactored to EventView and EventViewController to be more in line with CA

--- frontend/src/view/EventViewController.java
+++ frontend/src/view/EventViewController.java
@@ -2,7 +2,7 @@ package view;
 
 import java.time.LocalDateTime;
 
-public class EventViewData {
+public class EventViewController {
     private String eventName;
     private String eventType;
     private String dayStart;
@@ -11,56 +11,39 @@ public class EventViewData {
     private String timeEnd;
     private String priority;
 
-    public void setEventName(String eventName) {
+    public void saveEvent(String eventName, String eventType, String dayStart, String dayEnd,
+                         String timeStart, String timeEnd, String priority) {
         this.eventName = eventName;
+        this.eventType = eventType;
+        this.dayStart = dayStart;
+        this.dayEnd = dayEnd;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.priority = priority;
     }
 
     public String getEventName() {
         return eventName;
     }
 
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
     public String getEventType() {
         return eventType;
-    }
-
-    public void setDayStart(String dayStart) {
-        this.dayStart = dayStart;
     }
 
     public String getDayStart() {
         return dayStart;
     }
 
-    public void setDayEnd(String dayEnd) {
-        this.dayEnd = dayEnd;
-    }
-
     public String getDayEnd() {
         return dayEnd;
-    }
-
-    public void setTimeStart(String timeStart) {
-        this.timeStart = timeStart;
     }
 
     public String getTimeStart() {
         return timeStart;
     }
 
-    public void setTimeEnd(String timeEnd) {
-        this.timeEnd = timeEnd;
-    }
-
     public String getTimeEnd() {
         return timeEnd;
-    }
-
-    public void setEventPriority(String priority) {
-        this.priority = priority;
     }
 
     public String getPriority() {
