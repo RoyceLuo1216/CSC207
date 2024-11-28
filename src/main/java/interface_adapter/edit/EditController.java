8b892fc06afea67ec2def420af211e77d8c21621
Vennise Ho <venniseho14@gmail.com>
2024-11-27 23:08:33 -0500

    completed edit controller, input boundary, input data

--- src/main/java/interface_adapter/edit/EditController.java
+++ src/main/java/interface_adapter/edit/EditController.java
@@ -1,4 +1,44 @@
 package interface_adapter.edit;
 
+import usecase.edit.EditInputBoundary;
+import usecase.edit.EditInputData;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Controller for the Edit Use Case.
+ */
 public class EditController {
+    private final EditInputBoundary editInteractor;
+
+    public EditController(EditInputBoundary editInteractor) {
+        this.editInteractor = editInteractor;
+    }
+
+    /**
+     * Executes the Edit use case.
+     * @param eventName the name of the event
+     * @param eventType the type of the event
+     * @param dayStartString the start day of the event
+     * @param dayEndString the end day of the event
+     * @param timeStartString the start time of the event
+     * @param timeEndString the end time of the event
+     */
+    public void execute(String eventName, String eventType, String dayStartString, String dayEndString,
+                        String timeStartString, String timeEndString) {
+
+        DayOfWeek dayStart = DayOfWeek.valueOf(dayStartString.toUpperCase());
+        DayOfWeek dayEnd = DayOfWeek.valueOf(dayEndString.toUpperCase());
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h:mm a");
+        LocalTime timeStart = LocalTime.parse(timeStartString, timeFormatter);
+        LocalTime timeEnd = LocalTime.parse(timeEndString, timeFormatter);
+
+        final EditInputData editInputData = new EditInputData(eventName, eventType, dayStart, dayEnd, timeStart, timeEnd);
+
+        editInteractor.execute(editInputData);
+    }
 }
+
--- src/main/java/usecase/edit/EditInputData.java
+++ src/main/java/usecase/edit/EditInputData.java
@@ -1,4 +1,62 @@
 package usecase.edit;
 
+import java.time.LocalTime;
+import java.time.DayOfWeek;
+
+/**
+ * The input data for the Edit use case.
+ */
 public class EditInputData {
+    private final String eventName;
+    private final String eventType;
+    private final DayOfWeek dayStart;
+    private final DayOfWeek dayEnd;
+    private final LocalTime timeStart;
+    private final LocalTime timeEnd;
+
+    public EditInputData(String eventName, String eventType, DayOfWeek dayStart, DayOfWeek dayEnd,
+                         LocalTime timeStart, LocalTime timeEnd) {
+        this.eventName = eventName;
+        this.eventType = eventType;
+        this.dayStart = dayStart;
+        this.dayEnd = dayEnd;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+    }
+
+    String getEventName() {
+        return eventName;
+    }
+
+    String getEventType() {
+        return eventType;
+    }
+
+    DayOfWeek getDayStart() {
+        return dayStart;
+    }
+
+    DayOfWeek getDayEnd() {
+        return dayEnd;
+    }
+
+    LocalTime getTimeStart() {
+        return timeStart;
+    }
+
+    LocalTime getTimeEnd() {
+        return timeEnd;
+    }
+
+//    /**
+//     * Prints all instance variables.
+//     */
+//    public void getAll() {
+//        System.out.println(getEventName());
+//        System.out.println(getEventType());
+//        System.out.println(getDayStart());
+//        System.out.println(getDayEnd());
+//        System.out.println(getTimeStart());
+//        System.out.println(getTimeEnd());
+//    }
 }
