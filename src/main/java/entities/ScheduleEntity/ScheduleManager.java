fbfc300fae1b5b84da3abfa716e05fcbdff89ff7
Vennise Ho <venniseho14@gmail.com>
2024-11-30 13:34:59 -0500

    deleted all instances of FlexibleEvent

--- src/main/java/entities/ScheduleEntity/ScheduleManager.java
+++ src/main/java/entities/ScheduleEntity/ScheduleManager.java
@@ -2,7 +2,6 @@ package entities.ScheduleEntity;
 
 import entities.EventEntity.Event;
 import usecase.fixed.FixedEventUseCase;
-import usecase.flexible.FlexibleEventUseCase;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,22 +12,16 @@ import java.util.List;
 public class ScheduleManager {
     private final Schedule schedule;
     private final FixedEventUseCase fixedEventUseCase;
-    private final FlexibleEventUseCase flexibleEventUseCase;
-    //private final RepeatEventUseCase repeatEventUseCase;
+//    private final RepeatEventUseCase repeatEventUseCase;
 
     public ScheduleManager() {
         this.schedule = new Schedule();
         this.fixedEventUseCase = new FixedEventUseCase(schedule);
-        this.flexibleEventUseCase = new FlexibleEventUseCase(schedule);
         //this.repeatEventUseCase = new RepeatEventUseCase(schedule);
     }
 
     public boolean addFixedEvent(String name, int priority, LocalDateTime dayStart, LocalDateTime dayEnd) {
         return fixedEventUseCase.addFixedEvent(name, priority, dayStart, dayEnd);
-    }
-
-    public boolean addFlexibleEvent(String name, int priority, LocalDateTime dayStart, LocalDateTime dayEnd, float timeAllocation) {
-        return flexibleEventUseCase.addFlexibleEvent(name, priority, dayStart, dayEnd, timeAllocation);
     }
 
     public boolean addRepeatEvent(String name, int priority, LocalDateTime dayStart, LocalDateTime dayEnd) {
@@ -42,9 +35,5 @@ public class ScheduleManager {
 
     public List<Event> getAllEvents() {
         return schedule.getAllEvents();
-    }
-
-    public void optimizeFlexibleEvents() {
-        flexibleEventUseCase.optimizeFlexibleEvents();
     }
 }
--- src/main/java/factory/EventFactory.java
+++ src/main/java/factory/EventFactory.java
@@ -2,7 +2,6 @@ package factory;
 
 import entities.EventEntity.Event;
 import entities.EventEntity.FixedEvent;
-import entities.EventEntity.FlexibleEvent;
 import entities.EventEntity.RepeatEvent;
 
 import java.time.LocalDateTime;
@@ -11,10 +10,6 @@ import java.util.List;
 public class EventFactory {
     public static Event createFixedEvent(String eventName, int priorityLabel, LocalDateTime dayStart, LocalDateTime dayEnd) {
         return new FixedEvent(dayStart, dayEnd, eventName, priorityLabel);
-    }
-
-    public static Event createFlexibleEvent(String eventName, int priorityLabel, LocalDateTime dayStart, LocalDateTime dayEnd, float timeAllocation) {
-        return new FlexibleEvent(dayStart, dayEnd, eventName, priorityLabel, timeAllocation);
     }
 
     public static Event createRepeatEvent(LocalDateTime dayStart, LocalDateTime dayEnd, String eventName,
