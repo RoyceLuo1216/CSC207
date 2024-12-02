fbb3b85061819e539f86f45afa8a16b576358e7b
RoyceLuo1216 <86991110+RoyceLuo1216@users.noreply.github.com>
2024-12-01 21:17:06 -0500

    Refactoring for names and moving backtomain method to presenter for addEvent

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -55,7 +55,7 @@ public class AppBuilder {
     private ScheduleViewModel scheduleViewModel;
     private EditView editView;
     private EditViewModel editViewModel;
-    private repeatView
+    private RepeatView repeatView;
     private RepeatViewModel repeatViewModel;
 //    private RepeatView
     //private ScheduleViewModel scheduleViewModel;
